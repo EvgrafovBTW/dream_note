7ce1cec42dc9df4ce0e7cf2c8676e39b99576e92
Jennine Serra <jserra@student.21-school.ru>
2023-05-24 20:38:14 +0700

    added BETTER visibility in days where there are dreams

--- lib/screens/components/main_screen_calendar.dart
+++ lib/screens/components/main_screen_calendar.dart
@@ -4,58 +4,71 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class MainScreenCalendar extends StatelessWidget {
+class MainScreenCalendar extends StatefulWidget {
   const MainScreenCalendar({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  State<MainScreenCalendar> createState() => _MainScreenCalendarState();
+}
+
+class _MainScreenCalendarState extends State<MainScreenCalendar> {
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+
+  List<Dream> _getDreamsForDay(DateTime day) {
     DreamsBloc dreamsBloc = BlocProvider.of<DreamsBloc>(context);
     List<Dream> dreamList = dreamsBloc.state.dreams;
-    List<DateTime> dreamsDateList = [];
+    List<Dream> dreamsAtDayList = [];
     for (Dream d in dreamList) {
-      dreamsDateList.add(d.dreamDate.copyWith(
-          year: d.dreamDate.year,
-          month: d.dreamDate.month,
-          day: d.dreamDate.day,
-          hour: 0,
-          minute: 0,
-          second: 0,
-          millisecond: 0,
-          isUtc: true));
-      // print(dreamsDateList.last);
-      // print(dreamsDateList.last.isUtc);
+      if (d.dreamDate
+              .copyWith(
+                  year: d.dreamDate.year,
+                  month: d.dreamDate.month,
+                  day: d.dreamDate.day,
+                  hour: 0,
+                  minute: 0,
+                  second: 0,
+                  millisecond: 0,
+                  isUtc: true)
+              .compareTo(day) ==
+          0) {
+        dreamsAtDayList.add(d);
+      }
     }
+    return dreamsAtDayList;
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Column(
       children: [
         TableCalendar(
-          focusedDay: DateTime.now(),
           firstDay: DateTime(1900),
           lastDay: DateTime(2200),
+          focusedDay: _focusedDay,
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          },
           onFormatChanged: (format) {
             print(format);
           },
+          eventLoader: (day) {
+            return _getDreamsForDay(day);
+          },
           startingDayOfWeek: StartingDayOfWeek.monday,
-          calendarBuilders: CalendarBuilders(
-            defaultBuilder: (context, day, focusedDay) {
-              return Center(
-                child: dreamsDateList.contains(day)
-                    ? CircleAvatar(
-                        backgroundColor: Colors.deepPurpleAccent,
-                        child: Text(
-                          day.day.toString(),
-                        ))
-                    : Text(day.day.toString()),
-              );
-            },
-          ),
+          calendarBuilders: const CalendarBuilders(),
           calendarFormat: CalendarFormat.month,
           availableCalendarFormats: const {
             CalendarFormat.month: 'Month',
           },
-          // locale: "ru_RU",
         ),
       ],
     );
