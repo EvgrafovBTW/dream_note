35f9e99b6e16c91d833bded41aa9ad1139e9abe6
Jennine Serra <jserra@student.21-school.ru>
2023-05-24 17:16:54 +0700

    added visibility in days where there are dreams

--- lib/screens/components/main_screen_calendar.dart
+++ lib/screens/components/main_screen_calendar.dart
@@ -1,4 +1,7 @@
+import 'package:dream_note/logic/blocs/dreams/bloc/dreams_bloc.dart';
+import 'package:dream_note/models/dream_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class MainScreenCalendar extends StatelessWidget {
@@ -8,6 +11,23 @@ class MainScreenCalendar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    DreamsBloc dreamsBloc = BlocProvider.of<DreamsBloc>(context);
+    List<Dream> dreamList = dreamsBloc.state.dreams;
+    List<DateTime> dreamsDateList = [];
+    for (Dream d in dreamList) {
+      dreamsDateList.add(d.dreamDate.copyWith(
+          year: d.dreamDate.year,
+          month: d.dreamDate.month,
+          day: d.dreamDate.day,
+          hour: 0,
+          minute: 0,
+          second: 0,
+          millisecond: 0,
+          isUtc: true));
+      // print(dreamsDateList.last);
+      // print(dreamsDateList.last.isUtc);
+    }
+
     return Column(
       children: [
         TableCalendar(
@@ -18,7 +38,19 @@ class MainScreenCalendar extends StatelessWidget {
             print(format);
           },
           startingDayOfWeek: StartingDayOfWeek.monday,
-          calendarBuilders: CalendarBuilders(),
+          calendarBuilders: CalendarBuilders(
+            defaultBuilder: (context, day, focusedDay) {
+              return Center(
+                child: dreamsDateList.contains(day)
+                    ? CircleAvatar(
+                        backgroundColor: Colors.deepPurpleAccent,
+                        child: Text(
+                          day.day.toString(),
+                        ))
+                    : Text(day.day.toString()),
+              );
+            },
+          ),
           calendarFormat: CalendarFormat.month,
           availableCalendarFormats: const {
             CalendarFormat.month: 'Month',
